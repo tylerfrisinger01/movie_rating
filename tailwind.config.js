7490fa206602e356d23c0de207f2ab9db17dc97f
dkramer6 <dkramer6@huskers.unl.edu>
2025-03-03 21:38:18 -0600

    Fix css crash issues

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#f5f1e7',
+        foreground: '#d00000',
+      },
+    },
+  },
+  plugins: [],
+}
